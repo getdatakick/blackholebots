9ded2b796ac544ae065879e070c37d9f9ec15f60
Petr Hučík <petr.hucik@gmail.com>
2018-03-26 15:51:43 +0200

    fix constant definition

--- blackholebots.php
+++ blackholebots.php
@@ -1,5 +1,5 @@
 <?php
-define(BLACKHOLE_BOTS_ROOT, dirname(__FILE__));
+define('BLACKHOLE_BOTS_ROOT', dirname(__FILE__));
 require_once BLACKHOLE_BOTS_ROOT . '/classes/blackholebots.php';
 
 class BlackholeBots extends \Blackhole\BlackholeBotsCore {
